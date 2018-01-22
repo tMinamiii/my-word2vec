f663a1bdc38eba9318546aa45e276a91221c94ad
naronA <otherthan.new@gmail.com>
2018-01-22 22:29:16 +0900

    subsamplingを実装する

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def main():
     # print(sorted(token_gen)[0])
     token_gen = sorted(token_gen)[:settings.NUM_DOCUMENTS]
     # token_gen= [('aa','I want to eat an apple everyday'.split(' '))]
-
     w2v = word2vec.Word2Vec(window=settings.WINDOW,
                             alpha=settings.ALPHA,
                             size=settings.SIZE,
+                            sample=settings.SAMPLE,
                             codec='one_hot')
     w2v.make_model(token_gen)
     word = 'iphone'
@@ -23,10 +23,18 @@ def main():
         # token_gen = utils.find_and_load_token_files()
         random.shuffle(token_gen)
         w2v.train(token_gen)
-        most_similar = w2v.most_similar(word, rank)
         if i == 0 or (i + 1) % 5 == 0:
             print("epoch = {} -----------".format(i + 1))
-            print('{} =====> {}'.format(word, most_similar))
+            most_similar = w2v.most_similar(word, rank)
+            print_similar(word, most_similar)
+            # most_similar = w2v.most_similar_w(word, rank)
+            # print_similar(word, most_similar)
+
+
+def print_similar(word, most_similar):
+    print('{} ====='.format(word))
+    for i, (num, token) in enumerate(most_similar):
+        print('{} : {} \t {}'.format(i, token, num))
 
 
 if __name__ == '__main__':
--- w2v/word2vec.py
+++ w2v/word2vec.py
@@ -9,11 +9,14 @@ from w2v import neuralnet, settings
 
 
 class Vocabulary():
-    def __init__(self):
+    def __init__(self, sample):
+        self.total_token = 0
         self.seq_no_id = 0
         self.token_to_id = {}
         self.id_to_token = {}
+        self.subsampling_rate = {}
         self.counter = collections.Counter()
+        self.sample = sample
 
     def has(self, data):
         # 単語 or 単語リストが辞書にあるか無いかを判定する
@@ -26,18 +29,26 @@ class Vocabulary():
             return False
 
     def add(self, tokens):
+        self.total_token += len(tokens)
         self.counter.update(tokens)
 
     def build(self):
         # Counterを使って単語を出現頻度順に並べてある
+        def z(count):
+            return count / self.total_token
+        for _, count in self.counter.items():
+            if count <= settings.VOCABULARY_MIN_COUNT:
+                break
         for tok, count in self.counter.most_common():
-            if count >= settings.VOCABULARY_MAX_COUNT:
-                print(tok)
-                continue
             if count <= settings.VOCABULARY_MIN_COUNT:
                 break
             self.token_to_id[tok] = self.seq_no_id
             self.id_to_token[self.seq_no_id] = tok
+            subsample_rate = (np.sqrt(z(count) / self.sample) + 1.0) \
+                * self.sample / z(count)
+            if subsample_rate >= 1:
+                subsample_rate = 1.0
+            self.subsampling_rate[tok] = subsample_rate
             self.seq_no_id += 1
 
     def huffman(self, data):
@@ -77,15 +88,19 @@ class Vocabulary():
             dim = self.seq_no_id
             one_hot = np.zeros(dim)
             for t in data:
-                if self.has(t):
+                if self.has(t) and self.subsampling:
                     one_hot[self.token_to_id[t]] += 1.0
-            # return [one_hot / np.linalg.norm(one_hot)]
             return [one_hot]
+
+    def subsampling(self, token):
+        p = self.subsampling_rate[token]
+        return np.random.binomial(n=1, p=p)
 
 
 class Word2Vec():
     # Skip Gram Model
-    def __init__(self, window, alpha, size, codec='one_hot'):
+    def __init__(self, window, alpha, size,
+                 sample=0.001, codec='one_hot'):
         self.window_size = window
         self.nn = neuralnet.NeuralNetwork()
         self.alpha = alpha
@@ -93,16 +108,15 @@ class Word2Vec():
         self.codec = codec
         self.w = None
         self.w_ = None
-        self.vocab = None
+        self.vocab = Vocabulary(sample)
 
-    def vectorize(self, data):
+    def _vectorize(self, data):
         if self.codec == 'one_hot':
             return self.vocab.one_hot(data)
         elif self.codec == 'huffman':
             return self.vocab.huffman(data)
 
     def make_model(self, data):
-        self.vocab = Vocabulary()
         for tokens in data:
             self.vocab.add(tokens)
         self.vocab.build()
@@ -110,7 +124,7 @@ class Word2Vec():
         self.nn.prepare_model(self.alpha, self.size, voca_dim)
         self.nn.prepare_session()
 
-    def slice_window(self, tokens):
+    def _slice_window(self, tokens):
         ws = self.window_size
         for pos in range(len(tokens)):
             w_begin = pos - ws
@@ -133,10 +147,10 @@ class Word2Vec():
         for tokens in data:
             in_vecs = []
             out_vecs = []
-            for in_token, out_tokens in self.slice_window(tokens):
+            for in_token, out_tokens in self._slice_window(tokens):
                 if self.vocab.has(in_token) and self.vocab.has(out_tokens):
-                    in_vecs.extend(self.vectorize(in_token))
-                    out_vecs.extend(self.vectorize(out_tokens))
+                    in_vecs.extend(self._vectorize(in_token))
+                    out_vecs.extend(self._vectorize(out_tokens))
             q.put((np.array(in_vecs), np.array(out_vecs)))
         q.put((None, None))
         th.join()
@@ -147,52 +161,48 @@ class Word2Vec():
         self.w_ = th.w_
         return th.w, th.w_
 
-    def softmax(self, vec):
+    def _softmax(self, vec):
         vec_exp = np.exp(vec)
-        vec_sum = sum(vec_exp)
+        vec_sum = np.sum(vec_exp)
         return vec_exp / vec_sum
 
     def most_similar(self, token, rank):
-        code = self.vectorize(token)[0]
+        input_onehot = self._vectorize(token)[0]
+        wx = np.dot(input_onehot, self.w)
+        predict = np.dot(wx, self.w_)
+        predict /= np.linalg.norm(predict)
+        pred_softmax = self._softmax(predict)
+        print(np.sum(pred_softmax), np.max(pred_softmax))
+        # softmax関数に通して0-1の値にした後ソートして上位を類似ワードとする
+        sort = pred_softmax.argsort()[::-1]
+        most_similar = []
+        for i in sort[:rank]:
+            most_similar.append((predict[i], self.vocab.id_to_token[i]))
+        return most_similar
+
+    def _cos_sim(self, vec1, vec2):
+        vec1 /= np.linalg.norm(vec1)
+        vec2 /= np.linalg.norm(vec2)
+        cos_sim = np.dot(vec1, vec2.T)
+        return cos_sim
+
+    def most_similar_w_base(self, token, rank):
+        code = self._vectorize(token)[0]
         wx = np.dot(code, self.w)
-        predict = np.dot(wx.T, self.w_)
-        if False:
-            sort = predict.argsort()[::-1]
-            most_similar = [self.vocab.id_to_token[i] for i in sort[:rank]]
-            return most_similar
-        elif False:
-            most_similar = []
-            print(wx.T.shape)
-            softmax_wx = self.softmax(wx.T)
-            for i, vec in enumerate(self.w):
-                vec = self.softmax(vec)
-                cos_sim = np.dot(softmax_wx, vec.T)
-                most_similar.append((cos_sim, self.vocab.id_to_token[i]))
-                most_similar = sorted(most_similar, reverse=True)
-                most_similar = most_similar[:10]
-            return np.array(most_similar)[:, :]
-        elif False:
-            most_similar = []
-            predict_exp = np.exp(predict)
-            predict_sum = sum(predict_exp)
-            softmax = predict_exp / predict_sum
-            print(softmax.shape)
-            for tok, seq_no in self.vocab.token_to_id.items():
-                vec = self.vectorize(tok)[0]
-                vec = vec / np.linalg.norm(vec)
-                cos_sim = np.dot(softmax, vec.T)
-                most_similar.append((cos_sim, tok))
-                most_similar = sorted(most_similar, reverse=True)
-                most_similar = most_similar[:10]
-            return np.array(most_similar)[:, :]
-        elif True:
-            predict_exp = np.exp(predict)
-            predict_sum = sum(predict_exp)
-            softmax = predict_exp / predict_sum
-            sort = softmax.argsort()[::-1]
-            most_similar = [(softmax[i],
-                             self.vocab.id_to_token[i]) for i in sort[:rank]]
-            return most_similar
+        most_similar = []
+        # 重みベクトルWのコサイン類似度を測ったもの
+        token_id = self.vocab.token_to_id[token]
+        for i, vec in enumerate(self.w):
+            if i == token_id:
+                # 同じワードなら飛ばす
+                continue
+            wx /= np.linalg.norm(wx)
+            vec /= np.linalg.norm(vec)
+            cos_sim = np.dot(vec, wx.T)
+            most_similar.append((cos_sim, self.vocab.id_to_token[i]))
+            most_similar = sorted(most_similar, reverse=True)
+            most_similar = most_similar[:10]
+        return np.array(most_similar)[:, :]
 
 
 class SessionRunner(threading.Thread):
